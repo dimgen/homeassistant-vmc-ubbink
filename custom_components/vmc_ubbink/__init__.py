7e1578bdb7a64b081f74645baaebd451cec04d91
Dmitry <d.aleksashin@gmail.com>
2025-02-15 19:16:07 +0100

    Use modern Home Assistant async API for loading configs

--- custom_components/vmc_ubbink/__init__.py
+++ custom_components/vmc_ubbink/__init__.py
@@ -1,22 +1,35 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
 from .const import DOMAIN
 from .api import VMCUbifluxAPI
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+PLATFORMS = ["sensor", "select", "number"]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+
     hass.data.setdefault(DOMAIN, {})
 
-    api = VMCUbifluxAPI(
-        entry.data["host"],
-        entry.data["port"],
-        entry.data["username"],
-        entry.data["password"],
-    )
+    try:
+        api = VMCUbifluxAPI(
+            entry.data["host"],
+            entry.data["port"],
+            entry.data["username"],
+            entry.data["password"],
+        )
+    except Exception as err:
+        raise ConfigEntryNotReady from err
 
     hass.data[DOMAIN][entry.entry_id] = api
 
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "select"))
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "number"))
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
